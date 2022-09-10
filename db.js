6995f4369d09e3091fcafc07e9e65cde277eed51
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2022-09-10 18:21:57 +0300

    tr

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongodb://localhost:27017/game-of-clues');
+module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/game-of-clues');
