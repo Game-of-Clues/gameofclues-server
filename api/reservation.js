8e01366450d366ce11369332c1868bfc41dec52a
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2022-09-11 15:39:16 +0300

    changes

--- api/reservation.js
+++ api/reservation.js
@@ -31,16 +31,16 @@ router.post('/', (req, res, next) => {
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-      };
+    };
 
-      sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 
 })
 
