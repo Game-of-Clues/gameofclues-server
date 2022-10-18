1191fd67a070bc2a15979bf443ee9762ea66833c
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2022-10-18 17:22:17 +0300

    Authentication with JWT

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 const router = require('express').Router();
 const models = require('../models');
+const jwt = require('../modules/jwt');
 const userRouter = require('./user');
 const reservationRouter = require('./reservation');
 
 router.post('/register', (req, res, next) => {
-    const { email, firstName, lastName, password } = req.body;
-    models.User.create({ email, firstName, lastName, password })
-    .then(users => res.send(users))
-    .catch(next);
+    const { email, firstName, lastName, password, isAdmin } = req.body;
+    models.User.create({ email, firstName, lastName, password, isAdmin: isAdmin || false })
+        .then((user) => res.send(user))
+        .catch(next);
+});
+
+router.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+    models.User.findOne({ email }).then(user => {
+        if (!user) { res.send({ error: '[NOT_FOUND]' }); return; }
+        return Promise.all([user, jwt.create({ id: user._id })]);
+    }).then(([user, token]) => {
+        res.cookie('auth_cookie', token, { httpOnly: true });
+        res.send({ user });
+    }).catch(next);
 });
 
 router.get('/', (req, res) => {
